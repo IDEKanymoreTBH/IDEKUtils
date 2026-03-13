b6ae3a3904c4f7277adaa388fe1497656e9e7087
IDEKanymoreTBH <scottmason62445@gmail.com>
2026-03-13 18:15:09 -0500

    Update IDEKUtils.java
    Added sortToE and sortToELoss

--- IDEKUtils.java
+++ IDEKUtils.java
@@ -137,4 +137,142 @@ public class IDEKUtils {
         }
         return tempList;
     }
+    /**
+     * <h2>Overview:</h2>
+     * This Method Sorts An ArrayList Based On The Digits Of E.
+     * For Example, Any Twos Will Be First, Then Sevens, As They Occur In The Constant.
+     * This Method Is Primarily Used For A Single Thing, But Can Be Applied To Basically Any
+     * ArrayList That Only Has Integers. Doubles Are Not Allowed, Neither Other Numbers.
+     * Remember, E Only Contains Numbers, Not Extra Decimal Points Or The Letter L.
+     * <h2>Functionality:</h2>
+     * When Thinking Of This, It Is Best To Remember These Numbers: 2.71828182845904523536. This
+     * Is A Version Of E With Every Single Base 10 Number. I Choose To Ignore Zero
+     * Because Of A Previous Rule Basically Making It Useless. Here, We Can See The Hierarchy Of
+     * Numbers: 2, 7, 1, 8, 4, 5, 9, 3, 6.
+     * <h2>Examples:</h2>
+     * The List [4, 2, 1, 5, 4, 3, 4, 9, 6] Will Become [2, 1, 4, 4, 4, 5, 9, 3, 6]. There Is Also
+     * A Version Of This Function That Eliminates Duplicate Nums. That Is {@code sortToELoss}.
+     * @param originalList The Original List Of Numbers, Unsorted.
+     * @return A List Of Numbers That Have Been Sorted.
+     * @see #sortToELoss
+     */
+    public static ArrayList<Integer> sortToE(ArrayList<Integer> originalList) {
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 2) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 7) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 1) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 8) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 4) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 5) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 9) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 3) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 6) {
+                tempList.add(originalList.get(i));
+            }
+        }
+        return tempList;
+    }
+    /**
+     * <h2>Overview:</h2>
+     * This Function Acts Exactly Like The {@code sortToE}, Only It Removes Any Duplicate Values From The
+     * Original. It Is Recommended To Read About How {@code sortToE} Works Before This, Since This One
+     * Builds Off Of That One.
+     * <h2>Example:</h2>
+     * The List [6, 2, 8, 4, 4, 1, 9] Will Turn Into [2, 1, 8, 4, 9, 6], Removing The Duplicate 4. Pretty
+     * Self Explanatory.
+     * @param originalList The Original List In Its Unsorted Form
+     * @return The Sorted Version Of The List
+     * @see #sortToE
+     */
+    public static ArrayList<Integer> sortToELoss(ArrayList<Integer> originalList) {
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 2) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 7) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 1) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 8) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 4) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 5) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 9) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 3) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        for(int i = 0; i < originalList.size(); i++) {
+            if(originalList.get(i) == 6) {
+                tempList.add(originalList.get(i));
+                break;
+            }
+        }
+        return tempList;
+    }
 }
